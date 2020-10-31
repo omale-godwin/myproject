0cb2ded8a17a3011d0e5ff049a9e4738fa1b0196
omale-godwin <godwinpeace3@gmail.com>
2020-10-31 11:14:29 +0000

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `latest final year project topic and complete material`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+   
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,40 +8,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { FaBeer } from 'react-icons';
 
-import Header from "./header"
-import "./layout.css"
+import "./layout.css";
+import "./bootstrap.min.css";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+     <h1>that</h1>
+     {children}<FaBeer />
     </>
   )
 }
